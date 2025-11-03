b11ecab27f50e4d6619bb3c74dee28f93c8481c1
Tringi <tringi@trimcore.cz>
2025-11-03 03:50:41 +0100

    reshuffle some file operations

--- Notepad_File.cpp
+++ Notepad_File.cpp
@@ -36,38 +36,40 @@ struct Editor {
     std::set <std::size_t, std::size_t>     deletions;
 };
 
-bool File::init (HANDLE h) {
-    this->handle = h;
+bool File::init (HANDLE h) noexcept {
+    if (h != INVALID_HANDLE_VALUE) {
+        this->handle = h;
 
-    if (GetFileInformationByHandleEx (this->handle, FileIdInfo, &this->id, sizeof this->id)) {
-        this->id_type = ExtendedFileIdType;
+        if (GetFileInformationByHandleEx (this->handle, FileIdInfo, &this->id, sizeof this->id)) {
+            this->id_type = ExtendedFileIdType;
 
-    } else {
-        BY_HANDLE_FILE_INFORMATION info;
-        if (GetFileInformationByHandle (this->handle, &info)) {
-
-            this->id_type = FileIdType;
-            this->id.VolumeSerialNumber = info.dwVolumeSerialNumber;
-            std::memcpy (&this->id.FileId.Identifier [0], &info.nFileIndexLow, sizeof info.nFileIndexLow);
-            std::memcpy (&this->id.FileId.Identifier [4], &info.nFileIndexHigh, sizeof info.nFileIndexHigh);
-            std::memset (&this->id.FileId.Identifier [8], 0, sizeof this->id.FileId.Identifier - 8);
         } else {
-            this->id_type = MaximumFileIdType;
-        }
-    }
-    return true;
-}
+            BY_HANDLE_FILE_INFORMATION info;
+            if (GetFileInformationByHandle (this->handle, &info)) {
 
-bool File::init (const wchar_t * path) {
-    this->handle = CreateFile (path, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE,
-                               NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
-    if (this->handle != INVALID_HANDLE_VALUE) {
-        return this->init (this->handle);
+                this->id_type = FileIdType;
+                this->id.VolumeSerialNumber = info.dwVolumeSerialNumber;
+                std::memcpy (&this->id.FileId.Identifier [0], &info.nFileIndexLow, sizeof info.nFileIndexLow);
+                std::memcpy (&this->id.FileId.Identifier [4], &info.nFileIndexHigh, sizeof info.nFileIndexHigh);
+                std::memset (&this->id.FileId.Identifier [8], 0, sizeof this->id.FileId.Identifier - 8);
+            } else {
+                this->id_type = MaximumFileIdType;
+            }
+        }
+        return true;
     } else
         return false;
 }
 
-bool File::open (bool writable) {
+bool File::init (const wchar_t * path) noexcept {
+
+    // TODO: support passing file id like: <volume:id-bytes> for OpenFileById
+
+    return this->init (CreateFile (path, 0, FILE_SHARE_READ | FILE_SHARE_WRITE,
+                                   NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL));
+}
+
+bool File::open (bool writable) noexcept {
     if (this->id_type == MaximumFileIdType)
         return false;
 
@@ -99,6 +101,8 @@ bool File::open (bool writable) {
             if (size.QuadPart) {
 
                 // TODO: how to handle file that's too large?
+                // TODO: opening from network doesn't work, copy content
+
                 if (auto m = CreateFileMapping (h, NULL,
                                                 writable ? PAGE_READWRITE : PAGE_READONLY,
                                                 0, 0, NULL)) {
@@ -128,6 +132,17 @@ bool File::open (bool writable) {
     return false;
 }
 
+bool File::open (const wchar_t * path, bool writable) noexcept {
+    if (this->init (CreateFile (path, GENERIC_READ | (writable ? GENERIC_WRITE : 0), FILE_SHARE_READ,
+                                NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL))) {
+
+        // TODO: try mapping, then try copy
+
+        return true;
+    } else
+        return false;
+}
+
 void File::close () {
     this->id_type = MaximumFileIdType;
 
@@ -141,7 +156,7 @@ void File::close () {
     }
     if (this->handle) {
         CloseHandle (this->handle);
-        this->handle = NULL;
+        this->handle = INVALID_HANDLE_VALUE;
     }
 }
 
@@ -154,4 +169,22 @@ bool File::IsOpen (const FILE_ID_INFO * check) const noexcept {
         return false;
 
     return std::memcmp (check, &this->id, sizeof this->id) == 0;
+}
+
+wchar_t * File::GetCurrentFileName (wchar_t * buffer, DWORD length) const {
+    if (GetFinalPathNameByHandle (this->handle, buffer, length, 0)) {
+        DWORD offset = 0;
+
+        // remove prefixes
+        if (std::wcsncmp (buffer, L"\\\\?\\UNC\\", 8) == 0) {
+            buffer [6] = L'\\';
+            offset = 6;
+        } else
+        if (std::wcsncmp (buffer, L"\\\\?\\", 4) == 0) {
+            offset = 4;
+        }
+
+        return buffer + offset;
+    } else
+        return nullptr;
 }
--- Notepad_File.hpp
+++ Notepad_File.hpp
@@ -2,13 +2,15 @@
 #define NOTEPAD_FILE_HPP
 
 #include <Windows.h>
+#include <algorithm>
 
 class File {
 public:
     HANDLE handle = INVALID_HANDLE_VALUE;
 private:
-    HANDLE mapping = INVALID_HANDLE_VALUE;
+    HANDLE mapping = NULL;
     void * data = nullptr;
+    bool   allocated = false;
 
     FILE_ID_TYPE id_type = MaximumFileIdType;
 public:
@@ -18,6 +20,9 @@ public:
     File () = default;
     File (HANDLE h) noexcept {
         this->init (h);
+    }
+    ~File () {
+        this->close ();
     }
 
     File (File && f) noexcept
@@ -33,17 +38,30 @@ public:
         f.id_type = MaximumFileIdType;
     }
 
-    bool init (const wchar_t *);
-    bool init (HANDLE h);
+    File & operator = (File && other) noexcept {
+        std::swap (this->handle, other.handle);
+        std::swap (this->mapping, other.mapping);
+        std::swap (this->data, other.data);
+        std::swap (this->id_type, other.id_type);
+        std::swap (this->id, other.id);
+        return *this;
+    }
 
-protected:
-    bool open (bool writable);
+    bool init (HANDLE h) noexcept;
+    bool init (const wchar_t *) noexcept;
+    bool open (bool writable) noexcept;
+    bool open (const wchar_t *, bool writable) noexcept;
+
     void close ();
 
     bool IsOpen () const noexcept;
     bool IsOpen (const FILE_ID_INFO *) const noexcept;
 
+    wchar_t * GetCurrentFileName (wchar_t * buffer, DWORD length) const;
+
 private:
+    bool load ();
+
     File (const File &) = delete;
     File & operator = (const File &) = delete;
 };
